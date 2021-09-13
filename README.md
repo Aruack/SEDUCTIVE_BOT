3c849932f8fc20c19ce44cfd8829f058e41d31f7
Aruack <83180468+Aruack@users.noreply.github.com>
2021-09-13 21:00:02 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,20 @@
+# TagAllBot
+
+
+
+
+## DEPLOY ON HEROKU 🚀
+
+<p align="center"><a href="https://heroku.com/deploy?template=https://github.com/ARUACK/SEDUCTIVE_BOT"><img align="center" alt="Heroku" width="52px" src="https://www.nicepng.com/png/full/223-2233246_heroku-logo-salesforce-heroku.png"></p>
+ 
+
+
+
+
+## VARS
+
+```
+API_ID :- GET IT FROM MY.TELEGRAM.ORG 
+API_HASH :- GET IT FROM MY.TELEGRAM.ORG
+TOKEN :- GET IT FROM @BOTFATHER
+```
---
+++ bot.py
@@ -0,0 +1,163 @@
+import os, logging, asyncio
+
+from telegraph import upload_file
+
+from telethon import Button
+from telethon import TelegramClient, events
+from telethon.sessions import StringSession
+from telethon.tl.types import ChannelParticipantsAdmins
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(name)s - [%(levelname)s] - %(message)s'
+)
+LOGGER = logging.getLogger(__name__)
+
+api_id = int(os.environ.get("APP_ID"))
+api_hash = os.environ.get("API_HASH")
+bot_token = os.environ.get("TOKEN")
+client = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
+
+moment_worker = []
+
+
+#start
+@client.on(events.NewMessage(pattern="^/start$"))
+async def start(event):
+  await event.reply("^_^ Hey, Welcome To TAG Help Bot's Menu\nI can tag 15,000 Members in Group and 300 Members In Channel.\nNeed Help /help ",
+                    buttons=(
+                      [
+                         Button.url('📣 UPDATES', 'https://t.me/Aruackofficial'), 
+                         Button.url('⭐SUPPORT', 'https://t.me/Aruacksupport'), 
+                      ], 
+                      [
+                        Button.url('➕ ADD ME TO YOUR GROUP', 'https://t.me/SEDUCTIVE_BOT?startgroup=true'),   
+                      ]
+                   ), 
+                    link_preview=False
+                   )
+
+#help
+@client.on(events.NewMessage(pattern="^/help$"))
+async def help(event):
+  helptext = "**Tag Help Bot's Help Menu**\n\nCommand: /all \n You can use this command with text you want to tell others. \n`Example: /all Good morning!` \nYou can use this command as an answer. any message Bot will tag users to replied message"
+  await event.reply(helptext,
+                    buttons=(
+                      [
+                         Button.url('📣 UPDATES', 'https://t.me/Aruackofficial'), 
+                         Button.url('⭐SUPPORT', 'https://t.me/Aruacksupport'), 
+                      ], 
+                      [
+                        Button.url('➕ ADD ME TO YOUR GROUP', 'https://t.me/SEDUCTIVE_BOT?startgroup=true'),   
+                      ]
+                   ), 
+                    link_preview=False
+                   )
+
+
+@client.on(events.NewMessage(pattern="^/tagall|/call|/tall|/all|#all|@all?(.*)"))
+async def mentionall(event):
+  global moment_worker
+  if event.is_private:
+    return await event.respond("Use This In Channel or Group!")
+  
+  admins = []
+  async for admin in client.iter_participants(event.chat_id, filter=ChannelParticipantsAdmins):
+    admins.append(admin.id)
+  if not event.sender_id in admins:
+    return await event.respond("Only Admin can use it.")
+  
+  if event.pattern_match.group(1):
+    mode = "text_on_cmd"
+    msg = event.pattern_match.group(1)
+  elif event.reply_to_msg_id:
+    mode = "text_on_reply"
+    msg = event.reply_to_msg_id
+    if msg == None:
+        return await event.respond("I can't Mention Members for Old Post!")
+  elif event.pattern_match.group(1) and event.reply_to_msg_id:
+    return await event.respond("Give me can an Argument. Ex: `/tag Hey, Where are you`")
+  else:
+    return await event.respond("Reply to Message or Give Some Text To Mention!")
+    
+  if mode == "text_on_cmd":
+    moment_worker.append(event.chat_id)
+    usrnum = 0
+    usrtxt = ""
+    async for usr in client.iter_participants(event.chat_id):
+      usrnum += 1
+      usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
+      if event.chat_id not in moment_worker:
+        await event.respond("Stopped!")
+        return
+      if usrnum == 5:
+        await client.send_message(event.chat_id, f"{usrtxt}\n\n{msg}")
+        await asyncio.sleep(2)
+        usrnum = 0
+        usrtxt = ""
+        
+  
+  if mode == "text_on_reply":
+    moment_worker.append(event.chat_id)
+ 
+    usrnum = 0
+    usrtxt = ""
+    async for usr in client.iter_participants(event.chat_id):
+      usrnum += 1
+      usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
+      if event.chat_id not in moment_worker:
+        await event.respond("Stopped")
+        return
+      if usrnum == 5:
+        await client.send_message(event.chat_id, usrtxt, reply_to=msg)
+        await asyncio.sleep(2)
+        usrnum = 0
+        usrtxt = ""
+
+
+#telegraph 
+@client.on(events.NewMessage(pattern="^/t$"))
+async def telegraph(client, message):
+    replied = message.reply_to_message
+    if not replied:
+        await message.reply("Reply to a supported media file")
+        return
+    if not (
+        (replied.photo and replied.photo.file_size <= 5242880)
+        or (replied.animation and replied.animation.file_size <= 5242880)
+        or (
+            replied.video
+            and replied.video.file_name.endswith(".mp4")
+            and replied.video.file_size <= 5242880
+        )
+        or (
+            replied.document
+            and replied.document.file_name.endswith(
+                (".jpg", ".jpeg", ".png", ".gif", ".mp4"),
+            )
+            and replied.document.file_size <= 5242880
+        )
+    ):
+        await message.reply("Not supported!")
+        return
+    download_location = await client.download_media(
+        message=message.reply_to_message,
+        file_name="root/downloads/",
+    )
+    try:
+        response = upload_file(download_location)
+    except Exception as document:
+        await message.reply(message, text=document)
+    else:
+        await message.reply(
+            f"**Hey You...!\nLoook At This\n\n👉 https://telegra.ph{response[0]}**",
+            disable_web_page_preview=True,
+        )
+    finally:
+        os.remove(download_location)
+
+
+
+print("Started Successfully Join Support")
+print("¯\_(ツ)_/¯ Need Help Join @Aruacksupport")
+client.run_until_disconnected()
